f8b1f90a996eb6ccfd4905f432a48f15da1f4b87
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-02-06 12:13:21 +0100

    updated movie-View to use React.Component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,22 +1,33 @@
-export const MovieView = ({ movie, onBackClick }) => {
-    return (
-        <div>
-            <div>
-                <img src="movie.imageUrl" alt="" />
+import React from "react";
+
+export class MovieView extends React.Component {
+
+    render() {
+        const { movie, onBackClick} = this.props;
+        
+        return (
+            <div className="movie-view">
+                <div className="movie-poster">
+                    <img src="movie.imageUrl" alt="" />
+                </div>
+                <div className="movie-title">
+                    <span> Title: </span>
+                    <span>{movie.Title}</span>
+                </div>
+                <div className="movie-director">
+                    <span> Director: </span>
+                    <span>{movie.Director.Name}</span>
+                </div>
+                <div className="movie-genre">
+                    <span> Genre: </span>
+                    <span>{movie.Genre.Name}</span>
+                </div>
+                <div className="movie-description">
+                    <span> Description: </span>
+                    <span>{movie.Description}</span>
+                </div>
+                <button onClick={() => { onBackClick(null); }}>Back</button>
             </div>
-            <div>
-                <span> Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span> Director: </span>
-                <span>{movie.director.name}</span>
-            </div>
-            <div>
-                <span> Genre: </span>
-                <span>{movie.genre.name}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
-};
+        );
+    }
+}
