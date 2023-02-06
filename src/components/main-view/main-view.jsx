91025570e164c24eea68f26fcfb1cdbdc0f87b16
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-02-06 14:53:34 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
