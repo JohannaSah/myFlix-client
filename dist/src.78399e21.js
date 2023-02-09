fcbc064de9377c18ce7b561befceabb5f0e65b63
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-02-09 19:03:11 +0100

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -53411,7 +53411,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56316" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58354" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
